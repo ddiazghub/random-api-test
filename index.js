723e1f89c9461031323bdd134c4f31501a041570
Bob <bob@email.com>
2026-03-16 22:43:51 -0500

    add random int endpoint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const crypto = require("node:crypto");
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,10 @@ app.use(express.json());
  */
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!!!");
+});
+
+app.get("/random/int", (req, res) => {
+  res.status(200).send(crypto.randomInt(Number.MAX_SAFE_INTEGER));
 });
 
 app.listen(PORT, () => console.log("Server started on port:", PORT));
